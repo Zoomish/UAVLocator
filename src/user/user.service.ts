f2a9ed9504dd7ee2693397dff4d4c8202096c5e1
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2025-12-05 02:53:23 +0300

    `Removed IsNull import and updated checkLocations method to monitorCommentsInRealTime method`

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,7 +5,7 @@ import { Bot } from 'src/bot/entities/bot.entity'
 import { NoSessionService } from 'src/bot/services'
 import { TelegramClient } from 'telegram'
 import { StringSession } from 'telegram/sessions'
-import { IsNull, Not, Repository } from 'typeorm'
+import { Repository } from 'typeorm'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { User } from './entities/user.entity'
@@ -51,9 +51,8 @@ export class UserService {
     }
 
     async findAllWithLocation() {
-        return await this.userRepository.find({
-            where: [{ locations: Not(IsNull()) }, { locations: Not('[]') }],
-        })
+        const users = await this.findAll()
+        return users.filter((user) => user.locations.length > 0)
     }
 
     async update(tgId: number, dto: UpdateUserDto) {
@@ -61,57 +60,11 @@ export class UserService {
         return await this.userRepository.save(Object.assign(user, dto))
     }
 
-    async checkLocations() {
-        const apiId = this.configService.get('API_ID')
-        const apiHash = this.configService.get('API_HASH')
-        const session = this.configService.get('SESSION')
-        const client = new TelegramClient(
-            new StringSession(session),
-            +apiId,
-            apiHash,
-            {
-                connectionRetries: 5,
-            }
-        )
-        await client.connect()
-        try {
-            await client.getMe()
-        } catch (error) {
-            const admin = await this.findAdmin()
-            if (admin) {
-                this.noSessionService.NoSession(admin.tgId)
-            }
-        }
-        const users = await this.findAllWithLocation()
-        // Получаем канал
-        const channel = await client.getEntity('locatorru')
-
-        // Получаем все диалоги (чаты/каналы)
-        const dialogs = await client.getDialogs({})
-
-        // Находим наш канал в диалогах для получения непрочитанных
-        const channelDialog = dialogs.find((d) =>
-            d.entity.id.equals(channel.id)
-        )
-
-        if (!channelDialog) {
-            console.log('Канал не найден в диалогах')
-            await client.disconnect()
-            return
-        }
-
-        console.log(
-            `Непрочитанных сообщений в канале: ${channelDialog.unreadCount}`
-        )
-        for (const user of users) {
-        }
-        await client.disconnect()
-    }
     async monitorCommentsInRealTime() {
         const apiId = this.configService.get('API_ID')
         const apiHash = this.configService.get('API_HASH')
         const session = this.configService.get('SESSION')
-        const channelUsername = '@TestZoomishChannel'
+        const channelUsername = 'TestZoomishChannel'
         const client = new TelegramClient(
             new StringSession(session),
             +apiId,
@@ -120,36 +73,36 @@ export class UserService {
                 connectionRetries: 5,
             }
         )
-        await client.connect()
+        await client.connect().catch(async (e) => {
+            this.logger.error(e)
+            const admin = await this.findAdmin()
+            if (admin) {
+                this.noSessionService.NoSession(admin.tgId)
+            }
+        })
 
         const channel = await client.getEntity(channelUsername)
-        console.log(`📡 Мониторинг канала: ${channelUsername}`)
+        this.logger.log(`📡 Мониторинг канала: ${channelUsername}`)
 
-        // Обработчик новых сообщений
         client.addEventHandler(async (event) => {
             const message = event.message
-            if (
-                message
-            ) {
-                // Только новые посты (не реплаи)
+            if (message && channel.id.equals(message?.peerId?.channelId)) {
                 if (!message.replyTo) {
+                    const users = await this.findAllWithLocation()
+                    console.log(users)
+
                     console.log('\n📢 НОВЫЙ ПОСТ:')
                     console.log(`ID: ${message.id}`)
                     console.log(
                         `Текст: ${message.message?.substring(0, 200) || 'Нет текста'}`
                     )
                     console.log(`Дата: ${message.date}`)
-
-                    // Ваша логика обработки поста
-                    // saveToDatabase(message);
-                    // sendNotification(message);
                 }
             }
         })
 
         console.log('✅ Мониторинг запущен. Жду новые посты...')
 
-        // Держим соединение активным
         await client.connect()
     }
 }
