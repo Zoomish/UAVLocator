2b177ec02332edcb4031fc9a867cee0b35ebc6e4
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2026-01-29 02:04:13 +0300

    `Added UserService dependency injection and error handling for Telegram API 403 responses`

--- src/bot/services/handle/helpActions/sendInfo.service.ts
+++ src/bot/services/handle/helpActions/sendInfo.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, Logger } from '@nestjs/common'
+import { forwardRef, Inject, Injectable, Logger } from '@nestjs/common'
 import TelegramBot from 'node-telegram-bot-api'
+import { UserService } from 'src/user/user.service'
 
 @Injectable()
 export class SendInfoService {
+    constructor(
+        @Inject(forwardRef(() => UserService))
+        private readonly userService: UserService
+    ) {}
     private readonly logger = new Logger(SendInfoService.name)
 
     async sendInfo(tgId: number, message: string) {
@@ -11,6 +16,15 @@ export class SendInfoService {
             .sendMessage(tgId, message, {
                 parse_mode: 'HTML',
             })
-            .catch((e) => this.logger.error(e))
+            .catch((error) => {
+                if (error.response && error.response.statusCode === 403) {
+                    this.logger.warn(
+                        `User ${tgId} blocked the bot. Disabling the bot.`
+                    )
+                    this.userService.update(tgId, { notifications: false })
+                } else {
+                    this.logger.error(error)
+                }
+            })
     }
 }
