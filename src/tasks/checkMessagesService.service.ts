4097075afbbd48ead10cba074d4ce10624985961
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2026-02-08 08:08:20 +0300

    `Added onApplicationBootstrap lifecycle hook and updated CheckMessagesService`

--- src/tasks/checkMessagesService.service.ts
+++ src/tasks/checkMessagesService.service.ts
@@ -1,12 +1,15 @@
-import { Injectable, Logger } from '@nestjs/common'
+import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common'
 import { Interval } from '@nestjs/schedule'
 import { UserService } from '../user/user.service'
 
 @Injectable()
-export class CheckMessagesService {
+export class CheckMessagesService implements OnApplicationBootstrap {
     constructor(private readonly userService: UserService) {}
     private readonly logger = new Logger(CheckMessagesService.name)
 
+    async onApplicationBootstrap() {
+        await this.userService.checkUnreadMessages()
+    }
     @Interval(1000 * 60 * 2)
     async handleTimeout() {
         await this.userService.checkUnreadMessages()
