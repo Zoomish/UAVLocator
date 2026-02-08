0f7a781b35e4e0ef7c13be9c5b71acfa8a097277
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2026-02-08 07:57:34 +0300

    `Removed OnApplicationBootstrap implementation and moved checkUnreadMessages to handleTimeout`

--- src/tasks/checkMessagesService.service.ts
+++ src/tasks/checkMessagesService.service.ts
@@ -1,15 +1,12 @@
-import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { Interval } from '@nestjs/schedule'
 import { UserService } from '../user/user.service'
 
 @Injectable()
-export class CheckMessagesService implements OnApplicationBootstrap {
+export class CheckMessagesService {
     constructor(private readonly userService: UserService) {}
     private readonly logger = new Logger(CheckMessagesService.name)
 
-    async onApplicationBootstrap() {
-        await this.userService.checkUnreadMessages()
-    }
     @Interval(1000 * 60 * 2)
     async handleTimeout() {
         await this.userService.checkUnreadMessages()
